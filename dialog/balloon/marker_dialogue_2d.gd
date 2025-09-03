b921b2fdb6c7b918b1b19e2fcfb81374123b47d5
AlexOde <68616269+AlexOd4@users.noreply.github.com>
2025-09-03 17:18:15 +0200

    Improve dialogue marker direction logic and update scenes
    Refactored MarkerDialogue2D to better handle automatic direction and tracking of active markers. Updated test_dialog.dialogue with a new line, switched Signal02 to use a new dialogue resource in main.tscn, and added temporary scene files for editing.

--- dialog/balloon/marker_dialogue_2d.gd
+++ dialog/balloon/marker_dialogue_2d.gd
@@ -1,37 +1,40 @@
 class_name MarkerDialogue2D extends Marker2D
 
 var marker_offset: Marker2D
+var being_used: bool
+var markers_being_used: Array
+
 @export var automatic_direction: bool = true
-var being_used: bool:
-	set(value):
-		being_used = value
 @export var dialogue_offset: Vector2 = Vector2(100, 100):
 	get:
 		var offset: Vector2 = dialogue_offset
-		if automatic_direction:
-			var markers = get_tree().get_nodes_in_group("balloon_marker").filter(
-				func (c): return (c as MarkerDialogue2D).being_used
-			)
-			
-			for mark in markers.filter(func (c): return not c == self):
-				if self.global_position.direction_to(mark.global_position).x > 0:
-					offset.x = -offset.x
-				elif offset.x < 0: 
-					offset.x = -offset.x
-		
 		if marker_offset: 
 			offset = marker_offset.global_position - self.global_position
-		return offset
+		if automatic_direction:
+			for mark in markers_being_used:
+				if self.global_position.x - mark.global_position.x < 0:
+					offset.x = -offset.x
+				else: 
+					offset.x = abs(offset.x)
+		return offset 
 
 func _ready() -> void:
+	DialogueManager.dialogue_started.connect(_on_dialogue_started)
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+	
 	var children: Array[Node] = self.get_children().filter(func (c): return c as Marker2D)
 	if not children: return
 	marker_offset = children[0]
-	DialogueManager.dialogue_started.connect(_on_dialogue_started)
-	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
 
 func _on_dialogue_started(resource: DialogueResource) -> void:
-	if not resource.character_names.has(self.name) and not automatic_direction: return
-	being_used = true
+	if resource.character_names.has(self.name) and automatic_direction:
+		being_used = true
+		var set_markers_being_used :Callable = func():
+			markers_being_used = get_tree().get_nodes_in_group("balloon_marker").filter(
+			func(c): return c.being_used and not c == self
+			)
+		set_markers_being_used.call_deferred()
+		
+
 func _on_dialogue_ended(resource: DialogueResource) -> void: 
 	being_used = false
